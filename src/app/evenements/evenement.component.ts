ad57e39b8d47d87e2a385b364eab86c8da5ea424
Nicolas Huchet <nelgamix@gmail.com>
2017-08-18 10:34:56 +0200

    evenement max height

--- src/app/evenements/evenement.component.ts
+++ src/app/evenements/evenement.component.ts
@@ -1,13 +1,21 @@
-import {Evenement} from '../model/evenement';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 
-import {Component, Input} from '@angular/core';
+import {Evenement} from '../model/evenement';
 
 @Component({
   selector: 'evenement',
   templateUrl: './evenement.component.html',
   styleUrls: ['./evenement.component.css']
 })
-export class EvenementComponent {
+export class EvenementComponent implements AfterViewInit {
   @Input() ev: Evenement;
   @Input() source: any;
+
+  @ViewChild('evRow') evRow: ElementRef;
+
+  limitHeight: boolean;
+
+  ngAfterViewInit() {
+    setTimeout(() => this.limitHeight = this.evRow.nativeElement.offsetHeight > 200);
+  }
 }
