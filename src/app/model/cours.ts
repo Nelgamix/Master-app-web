53cf5b4373a09e2407eb37284d66967a002b2ee7
Nicolas Huchet <nelgamix@gmail.com>
2017-10-05 00:10:44 +0200

    amélioration du display des salles de l'et

--- src/app/model/cours.ts
+++ src/app/model/cours.ts
@@ -11,7 +11,9 @@ export class Cours {
   nom: string;
   professeur: string;
   description: string;
-  salle: string;
+
+  salles: any;
+  salles_types: string[];
 
   debut: any;
   fin: any;
@@ -25,11 +27,44 @@ export class Cours {
     this.nom = c.nom;
     this.professeur = c.professeur;
     this.description = c.description;
-    this.salle = c.salle;
+
+    // Analyse la salle (et le batiment)
+    this.salles = [];
+    const batreg = /(IM2AG|DLST)/g;
+    const sallereg = /(F[0-9]+)/g;
+    const stypereg = /(Amphi|TD|TP|PC)/ig;
+
+    const batm = [];
+    const sallem = [];
+    const stypem = stypereg.exec(c.salle);
+
+    let tmp;
+    while ((tmp = sallereg.exec(c.salle)) !== null) {
+      if (!sallem.includes(tmp[0])) {
+        sallem.push(tmp[0]);
+      }
+    }
+    while ((tmp = batreg.exec(c.salle)) !== null) {
+      if (!batm.includes(tmp[0])) {
+        batm.push(tmp[0]);
+      }
+    }
+
+    for (let i = 0; i < sallem.length; i++) {
+      this.salles.push({
+        salle: sallem[i],
+        batiment: i < batm.length ? batm[i] : ''
+      });
+    }
+
+    this.salles_types = stypem ? Array.from(new Set(stypem)) : []; // remove duplicates
+
+    // Début + fin
     this.debut = moment(c.debut, 'DD-MM-YYYY HH:mm');
     this.fin = moment(c.fin, 'DD-MM-YYYY HH:mm');
     this.duree = moment(c.duree, 'HH:mm');
 
+    // Couleur
     this.couleur = Cours.typeCouleurs[this.type];
     if (!this.couleur) {
       this.couleur = 'black';
